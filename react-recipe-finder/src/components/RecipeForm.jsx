fd9fdc115c39f230b94495ae7e25d54dab3b2ad0
Hackerdude374 <136039952+Hackerdude374@users.noreply.github.com>
2023-07-07 11:22:04 -0400

    Update RecipeForm.jsx with async action

--- react-recipe-finder/src/components/RecipeForm.jsx
+++ react-recipe-finder/src/components/RecipeForm.jsx
@@ -1,5 +1,38 @@
 
 import React, { useState } from 'react';
+import { Form, redirect } from "react-router-dom";
+
+export async function action({ handleAddRecipe }) {
+  const [name, setName] = useState('');
+  const [ingredients, setIngredients] = useState('');
+  const [instructions, setInstructions] = useState('');
+    const [image, setImage] = useState('');
+
+  const newRecipe = {
+    name,
+    ingredients: ingredients.split(',').map((ingredient) => ingredient.trim()),
+    instructions: instructions.split('\n').map((instruction) => instruction.trim()),
+    image,
+    status: 1,
+  };
+
+  const response = await fetch('http://localhost:3000/NewRecipeData', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newRecipe),
+  });
+  const addedRecipe = await response.json();
+   
+  handleAddRecipe(addedRecipe);
+  setName('');
+  setIngredients('');
+  setInstructions('');
+  setImage('');
+  return redirect('/');
+}
+
 
 //the recipe form will take 4 things: name, ingredients, and instructions.
 
