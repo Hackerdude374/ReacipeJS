eace9962fa49dc8e6b61521710e408f1d3db5a52
Hackerdude374 <136039952+Hackerdude374@users.noreply.github.com>
2023-07-07 11:22:55 -0400

    Update RecipeList.jsx

--- react-recipe-finder/src/components/RecipeList.jsx
+++ react-recipe-finder/src/components/RecipeList.jsx
@@ -1,15 +1,22 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
 import RecipeItem from './RecipeItem';
 
-
-
-
 function RecipeList({ recipes }) {
+  const [recipeList, setRecipeList] = useState(recipes);
+
+  useEffect(() => {
+    setRecipeList(recipes.filter((recipe) => recipeExists(recipe.id)));
+  }, [recipes]);
+
+  const recipeExists = (recipeId) => {
+   
+    return true;
+  };
+
   return (
-    <div className="recipe-list" >
-      {recipes.map((recipe, index) => (
-        <RecipeItem key={index} recipe={recipe} />
+    <div className="recipe-list">
+      {recipeList.map((recipe) => (
+        <RecipeItem key={recipe.id} recipe={recipe} />
       ))}
     </div>
   );
